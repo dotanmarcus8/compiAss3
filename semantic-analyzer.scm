7e0ebce7e323a7c26ab3dedfb3596d270402fc58
marcus dotan <mardot@cs302row1-1.cs.bgu.ac.il>
2017-12-16 12:40:36 +0200

    1

--- semantic-analyzer.scm
+++ semantic-analyzer.scm
@@ -1,6 +1,24 @@
 (define remove-applic-lambda-nil
-  ;; fill in the empty-lambda application elimination details here
-  )
+	(lambda (exp)
+		(cond
+			((not (list? exp)) 
+				exp)
+			((or (null? exp) (null? (car exp))) 
+				'())
+			((null? (cdr exp))
+				(cond 
+					((list? (car exp))
+						(list (map remove-applic-lambda-nil (car exp))))
+					(else
+						exp)))
+			((eq? 'applic (car exp))
+						(if (> (length (cadr (cadr exp))) 0)
+							(cons (car exp) (remove-applic-lambda-nil (cdr exp)))
+							(remove-applic-lambda-nil (caddr (cadr exp))))
+				)
+			(else 
+				(cons (car exp) (remove-applic-lambda-nil (cdr exp))))
+			)))
 
 (define box-set
   ;; fill in the variable boxing details here
